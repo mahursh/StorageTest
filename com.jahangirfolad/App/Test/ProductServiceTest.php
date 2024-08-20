794a90cf63a3c1914da72d84d12c601c502fcad7
Mahoor <mahoorshams3@gmail.com>
2024-08-20 15:50:20 +0330

    ProductServiceTest updated

--- com.jahangirfolad/App/Test/ProductServiceTest.php
+++ com.jahangirfolad/App/Test/ProductServiceTest.php
@@ -2,9 +2,24 @@
 
 namespace Test;
 
-class ProductServiceTest
-{
+use PDO;
+use Product;
+use Repository\ProductRepository;
+use Service\ProductService;
+
+
+require_once '../Repository/ProductRepository.php';
+require_once '../Entity/Product.php';
+require_once '../Service/ProductService.php';
 
 
 
-}
+$pdo = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+
+
+$repository = new ProductRepository($pdo);
+
+$service = new ProductService($repository);
+
+$product = new Product("rebar" , "kg");
+$service->saveProduct($product);
