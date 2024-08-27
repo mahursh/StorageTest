6f6e94a6887c84a8f796d61a35ab84718e3b7c30
Mahoor <mahoorshams3@gmail.com>
2024-08-27 11:14:57 +0330

    config Added

---
+++ com.jahangirfolad/ChangedApp/Config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("APPROOT", dirname(__DIR__));
+define("ROOT", dirname(dirname(__DIR__)));
+//define("URLROOT", "http://localhost/JahangirFoolad-backEnd-Sample");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "jf");
